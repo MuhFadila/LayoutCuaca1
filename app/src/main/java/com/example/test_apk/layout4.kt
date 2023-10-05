f7996d1265e36af45dfb651baa4cc74bf89fce4a
MuhFadila <muh.fadila18@gmail.com>
2023-10-05 17:32:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test_apk/layout4.kt
@@ -0,0 +1,11 @@
+package com.example.test_apk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout4)
+    }
+}
